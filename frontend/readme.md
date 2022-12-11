b0bc8e5cc1f62dc8d10107fe07fbd0fb8dcd1a67
Shubham Jangle <jangleshubham13@gmail.com>
2022-12-11 13:04:21 +0530

    bug fixes

---
+++ frontend/readme.md
@@ -0,0 +1,1 @@
+In vite env variables are defined prefixed `VITE_` and can be accessed using `import.meta.env`
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,10 @@ import { useEffect } from "react";
 
 function App() {
   const [data, setData] = useState("Loading...");
-  const url = "http://localhost:3000/";
+  const url = import.meta.env.VITE_REACT_APP_BACKEND_URL;
 
   useEffect(() => {
+    console.log();
     fetch(url)
       .then((res) => res.json())
       .then((data) => setData(data));
